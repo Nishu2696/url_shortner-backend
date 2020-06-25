b6b6b4141f3db530f44c33c91440dc3bee5888b2
Nishu2696 <nishaanth2696@gmail.com>
2020-06-25 19:39:13 +0530

    urlparser updated

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(function (req, res, next) {
 
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
+
 //const dbURL = `mongodb+srv://Nishu2696Url:Goku1996!@cluster0.fsljb.mongodb.net/myRegistration?retryWrites=true&w=majority`;
 const dbURL = `mongodb+srv://${process.env.D_USER}:${process.env.D_PASSWORD}@cluster0.fsljb.mongodb.net/myRegistration?retryWrites=true&w=majority`;
 //const dbURL = encode.uri(process.env.DBURL);
@@ -65,9 +66,9 @@ app.get('/urlshortnerapi', function (req, res) {
         if (err) throw err;
         let db = client.db("myRegistration");
         db.collection("url").find(function (err, result) {
-                console.log("result", result);
-                if (err) throw err;
-                res.send(result);
+            console.log("result", result);
+            if (err) throw err;
+            res.send(result);
         });
     });
 });
@@ -130,53 +131,46 @@ app.post('/createurlshortner', function (req, res) {
     //3rd try for shortening
 
 
+    console.log("getting longurl")
     mongoClient.connect(dbURL, (err, client) => {
         if (err) throw err;
         let urls = {
             longUrl: req.body.longUrl,
-            shortUrl: function () {
-                var rndResult = "";
-                var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-                var charactersLength = characters.length;
-
-                for (var i = 0; i < 5; i++) {
-                    rndResult += characters.charAt(
-                        Math.floor(Math.random() * charactersLength)
-                    );
-                }
-                console.log(rndResult);
-                return rndResult;
-            },
+            shortUrl: generateUrl(),
             count: 0
-        }
+        };
+        console.log("urls", urls);
         let db = client.db("myRegistration");
         db.collection("url").insertOne(urls, (err, data) => {
             console.log("hi longurl and shorturl has been inserted");
             if (err) throw err;
             console.log(data);
             client.close();
-            res.status(200).json({
-                msg: "Short URL successfully created"
-            });
+            res.send({
+                data: "short URL has been updated"
+            })
+            // res.status(200).json({
+            //     msg: "Short URL successfully created"
+            // });
         });
     });
 });
 
 //function to generate random letters and numbers and storing it in shorturl...
 
-// function generateUrl() {
-//     var rndResult = "";
-//     var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-//     var charactersLength = characters.length;
+function generateUrl() {
+    var rndResult = "";
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
 
-//     for (var i = 0; i < 5; i++) {
-//         rndResult += characters.charAt(
-//             Math.floor(Math.random() * charactersLength)
-//         );
-//     }
-//     console.log(rndResult)
-//     return rndResult
-// }
+    for (var i = 0; i < 5; i++) {
+        rndResult += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        );
+    }
+    console.log(rndResult)
+    return rndResult
+}
 
 //redirecting to the original login page with the help of short url link and updating the click count...
 
@@ -213,13 +207,13 @@ app.get('/:urlId', function (req, res) {
 })
 
 //creating an API for registration
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
     //connecting to the mongo
-    mongoClient.connect(dbURL, (err, client) => {
-        if (err) throw err;
-        //connecting to the database
+    const client = new mongoClient(dbURL, { useNewUrlParser: true });
+    try {
+        // Use connect method to connect to the Server
+        await client.connect();
         let db = client.db("myRegistration");
-        //using the collection from that particular database
         db.collection("users").findOne({ email: req.body.email }, async (err, data) => {
             if (err) throw err;
             if (data) {//if email already exists dont allow to create a new one
@@ -236,6 +230,7 @@ app.post("/register", (req, res) => {
                     phonenumber: req.body.phone_number,
                     active: false
                 }
+
                 let key = cryptoRandomString({ length: 10, type: "url-safe" });
                 let sessionLink = `${req.body.email}/ ${key}`;
                 let link = "http://localhost:4200/verificationemail/";
@@ -243,7 +238,6 @@ app.post("/register", (req, res) => {
                 let sent_to = req.body.email;
                 console.log("name_1", sent_to);
 
-                //create reusable transporter object using the default SMTP transport
                 let transporter = await nodemailer.createTransport({
                     //host: "smtp.ethereal.email",
                     //port: 587,
@@ -258,14 +252,13 @@ app.post("/register", (req, res) => {
                     }*/
                 });
 
-                // send mail with defined transport object
                 let info = await transporter.sendMail({
                     from: '"Nodemailer Contact" <marcnishaanth2696@gmail.com>', // sender address
                     to: `"${sent_to}", nishaanth2696@gmail.com`, // list of receivers
                     subject: "verification Link", // Subject line
                     text: "Hello world?", // plain text body
                     html: `<p>Please follow this link :</p></br>
-                           <a href=${link + sessionLink}>Click HERE</a>`, // html body
+                                           <a href=${link + sessionLink}>Click HERE</a>`, // html body
                 });
 
                 console.log("Message sent: %s", info.messageId);
@@ -273,31 +266,6 @@ app.post("/register", (req, res) => {
 
                 // Preview only available when sending through an Ethereal account
                 console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-                // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou..
-
-                // mongoClient.connect(dbURL, (err, client) => {
-                //     if (err) throw err;
-                //     let db = client.db("myRegistration");
-                //     db.collection("users").findOne({ email: req.body.email }, (err, data) => {
-                //         if (err) throw err;
-                //         if (data) {
-                //             console.log("name_1", sent_to);
-                //         }
-                //         client.close();
-                //     });
-
-                // })
-                // let mailStatus = verificationMail(
-                //     link,
-                //     req.body.email,
-                //     sessionLink
-                // );
-
-                // res.status(200).json({
-                //     message: "verification mail sent to: "+ req.body.email
-                // })
-                //if the email doesnt exist creating a new registration
-                //use of bcrypt for encrypting the password
 
                 bcrypt.genSalt(10, (err, salt) => {
                     console.log("password", req.body.password);
@@ -312,21 +280,28 @@ app.post("/register", (req, res) => {
                             console.log("hello");
                             if (err) throw err;
                             console.log(data);
-                            client.close();
                             res.status(200).json(data);
                         });
                     });
                 });
-
             }
-        })
-    });
+        });
+
+        //const db = client.db(dbName);
+    } catch (err) {
+        console.log(err.stack);
+    }
+    client.close();
 });
 
-app.post("/verificationemail", (req, res) => {
+app.post("/verificationemail", async (req, res) => {
     console.log("email", req.body.email);
-    mongoClient.connect(dbURL, (err, client) => {
-        if (err) throw err;
+    const client = new mongoClient(dbURL, { useNewUrlParser: true });
+
+    try {
+        // Use connect method to connect to the Server
+        await client.connect();
+
         let db = client.db("myRegistration");
         db.collection("users").findOne({ email: req.body.email }, (err, data) => {
             if (err) throw err;
@@ -338,12 +313,20 @@ app.post("/verificationemail", (req, res) => {
                 });
             }
         });
-    });
+        //const db = client.db(dbName);
+    } catch (err) {
+        console.log(err.stack);
+    }
+    client.close();
 })
 
-app.post("/login", (req, res) => {
-    mongoClient.connect(dbURL, (err, client) => {
-        if (err) throw err;
+app.post("/login", async (req, res) => {
+    const client = new mongoClient(dbURL, { useNewUrlParser: true });
+
+    try {
+        // Use connect method to connect to the Server
+        await client.connect();
+
         let db = client.db("myRegistration");
         db.collection("users").findOne({ email: req.body.email }, (err, data) => {
             if (err) throw err;
@@ -382,14 +365,60 @@ app.post("/login", (req, res) => {
                 });
             }
         })
-        /*db.collection("users").findOne({ email: req.body.email, password: req.body.password }, (err, data) => {
-            if (err) throw err;
-            client.close();
-        })*/
-    });
+        //const db = client.db(dbName);
+    } catch (err) {
+        console.log(err.stack);
+    }
+
+    client.close();
+    // mongoClient.connect(dbURL, (err, client) => {
+    //     if (err) throw err;
+    //     let db = client.db("myRegistration");
+    //     db.collection("users").findOne({ email: req.body.email }, (err, data) => {
+    //         if (err) throw err;
+    //         if (data.active == true) {
+    //             if (req.body.password == data.password) {
+    //                 res.status(200).json({
+    //                     msg: "success"
+    //                 });
+    //             }
+    //             else {
+    //                 res.status(401).json({
+    //                     msg: "Unauthorized / wrong Password"
+    //                 })
+    //             }
+    //             // console.log(req.body.password, "login");
+    //             // console.log(data.password, "register");
+    //             // bcrypt.compare(req.body.password, data.password, (err, result) => {
+    //             //     console.log(result);
+    //             //     if (err) throw err;
+    //             //     if (result) {
+    //             //         res.status(200).json({
+    //             //             msg: "Success"
+    //             //         });
+    //             //     }
+    //             //     else {
+    //             //         res.status(401).json({
+    //             //             msg: "Unauthorized / Wrong Password"
+    //             //         });
+    //             //     }
+    //             // });
+
+    //         }
+    //         else {
+    //             res.status(401).json({
+    //                 msg: "Verification not done yet/ Invalid E-mail"
+    //             });
+    //         }
+    //     })
+    //     /*db.collection("users").findOne({ email: req.body.email, password: req.body.password }, (err, data) => {
+    //         if (err) throw err;
+    //         client.close();
+    //     })*/
+    // });
 });
 
-app.post("/changepassword", (req, res) => {
+app.post("/changepassword", async (req, res) => {
 
     let random = Math.floor(Math.random() * 90000) + 10000;
     let key = cryptoRandomString({ length: 10, type: "url-safe" });
@@ -407,8 +436,12 @@ app.post("/changepassword", (req, res) => {
 
     let link = "http://localhost:4200/resetpassword/";
 
+    const client = new mongoClient(dbURL, { useNewUrlParser: true });
 
-    mongoClient.connect(dbURL, (err, client) => {
+    try {
+        // Use connect method to connect to the Server
+        await client.connect();
+
         let db = client.db("myRegistration");
         db.collection("users").findOne({ email: req.body.email }, async (err, data) => {
             if (err) throw err;
@@ -441,15 +474,64 @@ app.post("/changepassword", (req, res) => {
                 console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
                 // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou..
             }
-            client.close();
+            //client.close();
         });
+        //const db = client.db(dbName);
+    } catch (err) {
+        console.log(err.stack);
+    }
 
-    })
+    client.close();
+
+
+    // mongoClient.connect(dbURL, (err, client) => {
+    //     let db = client.db("myRegistration");
+    //     db.collection("users").findOne({ email: req.body.email }, async (err, data) => {
+    //         if (err) throw err;
+    //         if (data) {
+    //             //create reusable transporter object using the default SMTP transport
+    //             let transporter = await nodemailer.createTransport({
+    //                 service: "gmail",
+    //                 auth: {
+    //                     user: process.env.EMAIL, // generated ethereal user
+    //                     pass: process.env.PASSWORD, // generated ethereal password
+    //                 },
+    //             });
+
+    //             // send mail with defined transport object
+    //             let info = await transporter.sendMail({
+    //                 from: '"Nodemailer Contact" <marcnishaanth2696@gmail.com>', // sender address
+    //                 to: `"${sent_to}", nishaanth2696@gmail.com`, // list of receivers
+    //                 subject: "Hello ✔", // Subject line
+    //                 text: "Hello world?", // plain text body
+    //                 html: `<p>Please follow this link :</p></br>
+    //                         <p>${link + sent_to}</p>
+    //                        <a href=${link + sent_to}>Click HERE</a>`, // html body
+    //                 //html: `<b>"${random}"</b>`, // html body
+    //             });
+
+    //             console.log("Message sent: %s", info.messageId);
+    //             // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    //             // Preview only available when sending through an Ethereal account
+    //             console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    //             // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou..
+    //         }
+    //         client.close();
+    //     });
+
+    // })
 
 });
 
-app.post("/interchangepassword", (req, res) => {
-    mongoClient.connect(dbURL, (err, client) => {
+app.post("/interchangepassword",async (req, res) => {
+
+    const client = new mongoClient(dbURL, { useNewUrlParser: true });
+
+    try {
+        // Use connect method to connect to the Server
+        await client.connect();
+
         if (err) throw err;
         let db = client.db("myRegistration");
         db.collection("users").findOne({ email: req.body.email }, (err, data) => {
@@ -472,5 +554,35 @@ app.post("/interchangepassword", (req, res) => {
                 });
             }
         });
-    });
+
+        //const db = client.db(dbName);
+    } catch (err) {
+        console.log(err.stack);
+    }
+
+    client.close();
+    // mongoClient.connect(dbURL, (err, client) => {
+    //     if (err) throw err;
+    //     let db = client.db("myRegistration");
+    //     db.collection("users").findOne({ email: req.body.email }, (err, data) => {
+    //         if (err) throw err;
+    //         if (data) {
+    //             bcrypt.genSalt(10, (err, salt) => {
+    //                 bcrypt.hash(req.body.password, salt, function (err, hash) {
+    //                     req.body.password = hash;
+    //                     db.collection("users").updateOne({ email: req.body.email }, { $set: { password: req.body.password } }, (err, data) => {
+    //                         if (err) throw err;
+    //                         client.close();
+    //                         res.status(200).json(data);
+    //                     });
+    //                 });
+    //             });
+    //         }
+    //         else {
+    //             res.status(401).json({
+    //                 msg: "Invalid E-mail"
+    //             });
+    //         }
+    //     });
+    // });
 });
