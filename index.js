83c2e1cda5ad2103a933ba3cb665369d33698217
Nishu2696 <nishaanth2696@gmail.com>
2020-08-07 14:42:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    if (req.method == 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,PATCH");
+        return res.status(200).json({});
+    }
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
